c4a15c371503794bd59acf47555104c3a8b0dbe5
Rennie McIntyre <rennie.mcintyre@sky.uk>
2022-04-30 17:44:57 +0100

    Updated with methods

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
-// var EdgeDB = require('./lib/edgedb');
-// exports.EdgeDB = () => {
-//     return 'HELLO WORLD';
-// }
-
 export default class EdgeDB {
 
-    constructor(message) {
-        this.message = message
+    constructor(API_KEY, DB_URL) {
+        this.API_KEY = API_KEY;
+        this.DB_URL = DB_URL;
     }
 
-    getMessage(){
-        return `Hello world!! ${this.message}`
+    async find(options){
+
+        const path = '/find';
+        const url = this.DB_URL + path;
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(options)
+        })
+
+        return await response.json();
+
     }
+
+
 }
