7b8747b672ab46a56dddd79e2ac8f212e0597a95
Rennie McIntyre <rennie.mcintyre@sky.uk>
2022-04-30 18:44:34 +0100

    Added find methods

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import fetch from 'node-fetch';
+
 export default class EdgeDB {
 
     constructor(API_KEY, DB_URL) {
@@ -12,9 +14,29 @@ export default class EdgeDB {
         const response = await fetch(url, {
             method: "POST",
             headers: {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.API_KEY
             },
             body: JSON.stringify(options)
+        })
+
+        return await response.json();
+
+    }
+
+    async findOne(id){
+
+        const path = '/findOne';
+        const url = this.DB_URL + path;
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + this.API_KEY
+            },
+            body: JSON.stringify({
+                id: id
+            })
         })
 
         return await response.json();
